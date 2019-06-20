fc0cfc6e9de1cedd6b12181b33638359be383bd9
Utkarsh Tiwari <iamutkarshtiwari@gmail.com>
2019-06-20 15:37:44 +0900

    Add randomize function to roll

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var randomDiceIndexLeft: Int = 0
+    var randomDiceIndexRight: Int = 0
 
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func rollButton(_ sender: UIButton) {
+        
+        diceImageViewLeft.image = getRandomDiceImage()
+        diceImageViewRight.image = getRandomDiceImage()
+    }
+    
+    private func getRandomDiceImage() -> UIImage {
+        let number = Int.random(in: 0 ... 5) + 1
+        return UIImage(named: "dice\(number)")!
+    }
 
 }
 
